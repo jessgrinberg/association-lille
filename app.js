0be4c66df35d3e906c69e7bcb552201358ca3735
jessgrinberg <jessgrinberg@gmail.com>
2017-12-01 23:35:14 -0800

    added carousel next/previous

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-$('.carousel').carousel()
-
 $(window).scroll(function() {
     if($(this).scrollTop()> $( window ).height() ) {
         $( ".navbar-me" ).addClass("fixed-me");
     } else {
         $( ".navbar-me" ).removeClass("fixed-me");
     }
+});
+
+$(document).ready(function () {
+    $('#myCarousel').carousel({
+        interval: 4000
+    })
+    $('.carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
 });
